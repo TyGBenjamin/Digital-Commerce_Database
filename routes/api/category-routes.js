913e652463d1d080ccf1b663b929d162921f7c94
Tyler Gandy <tylerbgandy@gmail.com>
2022-03-08 13:10:54 -0500

    Made changes to Categories route to get proper functionality

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,31 +5,28 @@ const { Category, Product } = require("../../models");
 
 router.get("/", async (req, res) => {
   try {
-    await Category.findAll({
-      include: [Product],
-    }).then((categoryData) => {
-      res.json(categoryData);
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
     });
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-  // find all categories
-  // be sure to include its associated Products
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    await Category.findByPk(req.params.id, {
-      include: [Product],
-    }).then((data) => {
-      res.json(data);
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
     });
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found with that id!" });
+      return;
+    }
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
   }
-
-  // find one category by its `id` value
-  // be sure to include its associated Products
 });
 
 router.post("/", async (req, res) => {
