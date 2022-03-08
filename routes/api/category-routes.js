d0bfa9e100b3b8433dfd422ef758cab48d2962a5
Tyler Gandy <tylerbgandy@gmail.com>
2022-03-08 13:41:08 -0500

    several changes made to category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,20 +32,52 @@ router.get("/:id", async (req, res) => {
 router.post("/", async (req, res) => {
   // create a new category
   try {
-    await Category.create(req.body).then((newData) => {
+    const newData = await Category.create({category_name = req.category_name});
       res.json(newData);
-    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedCategory = await Category.update(
+      {
+        // All the fields you can update and the data attached to the request body.
+        category_name: req.body.category_name,
+      },
+      {
+        // Gets a category based on the category_id given in the request parameters
+        where: {
+          category_id: req.params.category_id,
+        },
+      }
+    );
+    res.json(updatedCategory);
+  } catch (err) {
+    res.status(404).json({ message: ` Update Unsuccessful` });
+  }
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+
+  try {
+    const deletedCategory = await Category.destroy({
+      // Gets a category based on the category_id given in the request parameters
+      where: {
+        category_id: req.params.category_id,
+      },
+    });
+    if (!deletedCategory) {
+      res
+        .status(404)
+        .json({ message: `Delete Unsucessful, no category with that id` });
+    }
+    res.status(200).json(deletedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
